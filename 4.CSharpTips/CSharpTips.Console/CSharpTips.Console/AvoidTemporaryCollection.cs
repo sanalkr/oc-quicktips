6a1d0f4f3623864dc03beef186eed40aa81c2630
sanalkr <sanals@live.in>
2023-01-27 11:44:51 +0530

    CSharp Tips

---
+++ 4.CSharpTips/CSharpTips.Console/CSharpTips.Console/AvoidTemporaryCollection.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace CSharpTips.Console
+{
+
+    public class AvoidTemporaryCollection
+    {
+        private IEnumerable<int> _temporaryCollection = Enumerable.Range(50, 100);   
+    
+
+        public IEnumerable<int> GetValuesAboveHundredWithTempCollection()
+        {
+            List<int> tempCollection = new List<int>();
+
+            foreach (var item in _temporaryCollection)
+            {
+                if(item > 100)
+                    tempCollection.Add(item);
+            }
+ 
+            return tempCollection;
+        }
+
+        public IEnumerable<int> GetValuesAboveHundred()
+        {
+            foreach (var item in _temporaryCollection)
+            {
+                if (item > 100)
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ 4.CSharpTips/CSharpTips.Console/CSharpTips.Console/SuperHeroes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTips.Console
+{
+    public class SuperHeroes
+    {
+        List<string> _heroes = new List<string>();
+
+        public SuperHeroes()
+        {
+            _heroes.Add("Tony");
+            _heroes.Add("Steve");
+            _heroes.Add("Thor");
+            _heroes.Add("Bruce");
+            _heroes.Add("Natasha");
+            _heroes.Add("Barton");
+
+        }
+
+        public IEnumerable<string> Avengers { get { return _heroes.AsReadOnly();  } }
+    }
+}
